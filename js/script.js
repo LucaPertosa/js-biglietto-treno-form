b0ff2c895df8cacd2589e8297734a1b58f1212d6
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-09 20:03:43 +0100

    Milestone 2 completato

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-const sumbitBtn = document.getElementById("submit-btn");
-sumbitBtn.addEventListener("click", function() {
-    const result = document.getElementById("result");
-    // Ripulire il result
-    // result.className = "";
+// Bottone sub
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", function() {
 
     // Prendere il valore del input.
+    const userNameInput = document.getElementById("user-name");
+    const userName = userNameInput.value;
+
     const userDistanceInput = document.getElementById("distance");
     const userDistance = (userDistanceInput.value);
 
     const userAgeInput = document.getElementById("user-age");
     const userAge = userAgeInput.value;
 
-    console.log(userDistance, userAge);
+    console.log(userName, userDistance, userAge);
 
     /* prezzo biglietto euro/km */
     const priceKm = 0.21 ;  
@@ -20,32 +21,53 @@ sumbitBtn.addEventListener("click", function() {
     /* sconto 40% < 65 anni */
     const discount40 = 0.40 ;
 
-    // Stampo le scelte dell'utente
-
     // Calcolo prezzo del biglietto in base ai chilometri
     const price = userDistanceInput.value * priceKm
     console.log(price);
     // Calcolo sconto in base all'età e lo applico al prezzo
     let finalPrice = '';
+    let userDiscount = '';
 
     if (userAge === 'min18'){
         finalPrice = (price - (discount20 * price)).toFixed(2)
+        userDiscount = "20% di sconto"
     } else if (userAge === 'over65') {
         finalPrice = (price - (discount40 * price)).toFixed(2)
+        userDiscount = "40% di sconto"
     } else {
         finalPrice = price.toFixed(2)
+        userDiscount = "Nessuno sconto"
     }
-    // Preparo il messaggio con il prezzo
-    console.log(finalPrice);
 
-    // Output messaggio
-    document.getElementById('message').innerHTML = `Grazie per aver utilizzato il calcolatore! Il prezzo del tuo biglietto è di € ${finalPrice} ` ; 
+    // Randomizzazione carrozza e CP
+    let randomCp = Math.floor(Math.random() * 100000);
+    let randomCarrozza = Math.floor(Math.random() * 9 + 1);
 
+    // Inserimento dati in tabella
+    document.getElementById("tab-user-name").innerHTML = `${userName}`
+    document.getElementById("offerta").innerHTML = `${userDiscount}`
+    document.getElementById("carrozza").innerHTML = `${randomCarrozza}`
+    document.getElementById("cp").innerHTML = `${randomCp}`
+    document.getElementById("final-price").innerHTML = `€ ${finalPrice}`
 
-    result.innerHTML = `${finalPrice}`;
-    result.classList.add();
+});
 
+// Bottone cancel
+const cancelBtn = document.getElementById("cancel-btn");
+cancelBtn.addEventListener("click", function() {
+
+    const userNameInput = document.getElementById("user-name");
+    const userDistanceInput = document.getElementById("distance");
+    const userAgeInput = document.getElementById("user-age");
     // Ripulisco i campi di input
+    userNameInput.value = "";
     userDistanceInput.value = "";
     userAgeInput.value = "";
+
+    // Ripulisco i campi tabella
+    document.getElementById("tab-user-name").innerHTML = ``
+    document.getElementById("offerta").innerHTML = ``
+    document.getElementById("carrozza").innerHTML = ``
+    document.getElementById("cp").innerHTML = ``
+    document.getElementById("final-price").innerHTML = ``
 });
